e02feb5b079f2a8843a38968bff447a11eaad855
fe-berrios <fe.berrios.ov@gmail.com>
2025-03-30 19:50:00 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'proyectoCasero',
+  webDir: 'www'
+};
+
+export default config;
