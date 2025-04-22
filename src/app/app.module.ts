6700e9b4f3bb5f320d7f8abff21c7cfbb9f4c8f2
fe-berrios <fe.berrios.ov@gmail.com>
2025-04-21 20:03:49 -0400

    Se implemento firebase y tiene un service de creación de feria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { environment } from 'src/environments/environment.prod';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, 
+            IonicModule.forRoot(), 
+            AppRoutingModule,
+            AngularFireModule.initializeApp(environment.firebase),
+            AngularFirestoreModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase : {
+  apiKey: "AIzaSyB8xMyQrCNu7Ih8U-vqYMxmclzSyudyLDI",
+  authDomain: "casero-2bcea.firebaseapp.com",
+  projectId: "casero-2bcea",
+  storageBucket: "casero-2bcea.firebasestorage.app",
+  messagingSenderId: "867915119773",
+  appId: "1:867915119773:web:7b51e06a9e04e4bc74a874"
+  }
 };
