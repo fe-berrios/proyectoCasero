15e7f27741e98deef9ad62f7cedcc86affcc9366
Martín <133818741+xrev4n@users.noreply.github.com>
2025-04-26 14:56:52 -0400

    Integra supabase + login
    - Integra supabase
    - Login usando magic link
    - Aun no funcional, falta account page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
+import { SupabaseService } from 'src/app/services/supabase.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   standalone: false,
@@ -8,9 +10,47 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
-  constructor(private router: Router) {}
+  loading = false;
+  signInForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+  });
 
-  navigateTo(path: string) {
-    this.router.navigate([path]);
+  constructor(
+    private formBuilder: FormBuilder,
+    private supabase: SupabaseService,
+    private alertController: AlertController
+  ) {}
+
+  // Método para manejar el envío del formulario de inicio de sesión
+  async onSubmit(): Promise<void> {
+    try {
+      this.loading = true;
+      const email = this.signInForm.value.email as string;
+
+      // Llamada al servicio de Supabase para enviar el Magic Link
+      const { error } = await this.supabase.signIn(email);
+      if (error) throw error;
+
+      // Mostrar alerta de éxito
+      const alert = await this.alertController.create({
+        header: 'Correo enviado',
+        message: 'Revisa tu bandeja de entrada para continuar con el inicio de sesión.',
+        buttons: ['OK'],
+      });
+      await alert.present();
+    } catch (error) {
+      if (error instanceof Error) {
+        // Mostrar alerta de error
+        const alert = await this.alertController.create({
+          header: 'Error',
+          message: error.message,
+          buttons: ['OK'],
+        });
+        await alert.present();
+      }
+    } finally {
+      this.signInForm.reset();
+      this.loading = false;
+    }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
+  production: false,
+  supabaseUrl: 'https://huamcrsihvdtbwrijaly.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1YW1jcnNpaHZkdGJ3cmlqYWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2ODc4NzIsImV4cCI6MjA2MTI2Mzg3Mn0.TdA-uq17-WlcG3pTxnKwp0MNxXLaHVdUS2FotHHArlM',
+}
 
 /*
  * For easier debugging in development mode, you can import the following file
