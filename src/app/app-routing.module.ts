28677c821cdbb63093522dbff9482a2a86fe6280
Martín <133818741+xrev4n@users.noreply.github.com>
2025-04-26 15:41:28 -0400

    Integra account page
    - Integra pagina de perfil
    - Falta probar compilando la app (error con los tokens en el navegador)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'registro',
     loadChildren: () => import('./pages/registro/registro.module').then( m => m.RegistroPageModule)
+  },  {
+    path: 'account',
+    loadChildren: () => import('./account/account.module').then( m => m.AccountPageModule)
   }
+
 ];
 
 @NgModule({
