42c2d41039b8ae8722efceb9e52c933765b77847
Martín <133818741+xrev4n@users.noreply.github.com>
2025-04-26 16:14:04 -0400

    Gitignore enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://huamcrsihvdtbwrijaly.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1YW1jcnNpaHZkdGJ3cmlqYWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2ODc4NzIsImV4cCI6MjA2MTI2Mzg3Mn0.TdA-uq17-WlcG3pTxnKwp0MNxXLaHVdUS2FotHHArlM',
 };
