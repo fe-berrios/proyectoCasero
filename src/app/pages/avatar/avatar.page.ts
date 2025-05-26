58cc700e47ff4be340217e03515e8cff5ec68b48
Martín <133818741+xrev4n@users.noreply.github.com>
2025-05-26 00:29:56 -0400

    Añade mas avatares

--- src/app/pages/avatar/avatar.page.ts
+++ src/app/pages/avatar/avatar.page.ts
@@ -13,7 +13,22 @@ export class AvatarPage {
   avatars = [
     { name: 'joy', src: 'assets/profile_pics/joy.png' },
     { name: 'angry', src: 'assets/profile_pics/angry.png' },
-    { name: 'sad', src: 'assets/profile_pics/sad.png' }
+    { name: 'sad', src: 'assets/profile_pics/sad.png' },
+    { name: 'kabuki', src: 'assets/profile_pics/kabuki.png' },
+    { name: 'sunglasses', src: 'assets/profile_pics/sunglasses.png' },
+    { name: 'thief', src: 'assets/profile_pics/thief.png' },
+    { name: 'cowboy', src: 'assets/profile_pics/cowboy.png' },
+    { name: 'mafia', src: 'assets/profile_pics/mafia.png' },
+    { name: 'fire', src: 'assets/profile_pics/fire.png' },
+    { name: 'pirate', src: 'assets/profile_pics/pirate.png' },
+    { name: 'girl', src: 'assets/profile_pics/girl.png' },
+    { name: 'girl-sunglasses', src: 'assets/profile_pics/girl-sunglasses.png' },
+    { name: 'goth', src: 'assets/profile_pics/goth.png' },
+    { name: 'sailor', src: 'assets/profile_pics/sailor.png' },
+    { name: 'witch', src: 'assets/profile_pics/witch.png' },
+    { name: 'skull', src: 'assets/profile_pics/skull.png' },
+    { name: 'cat', src: 'assets/profile_pics/cat.png' },
+    { name: 'farmer', src: 'assets/profile_pics/farmer.png' },
   ];
 
   selectedAvatar: string = '';
