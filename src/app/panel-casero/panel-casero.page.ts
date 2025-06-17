7a8382094665952888a83b6f94de734000547524
Martín <133818741+xrev4n@users.noreply.github.com>
2025-06-17 14:52:58 -0400

    Update panel-casero.page.ts

--- src/app/panel-casero/panel-casero.page.ts
+++ src/app/panel-casero/panel-casero.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  standalone: false,
   selector: 'app-panel-casero',
   templateUrl: './panel-casero.page.html',
   styleUrls: ['./panel-casero.page.scss'],
