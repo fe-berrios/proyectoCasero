5db075626b244fbf8eebd92c8a623dc78c02cd6f
Martín <133818741+xrev4n@users.noreply.github.com>
2025-06-17 18:13:38 -0400

    Acordeon de comentarios

--- src/app/components/feria-modal/feria-modal.component.ts
+++ src/app/components/feria-modal/feria-modal.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { PuestoService } from 'src/app/services/puesto.service';
 import { PuestoDetalleModalComponent } from '../puesto-detalle-modal/puesto-detalle-modal.component';
+import { ComentarFeriaComponent } from '../comentar-feria/comentar-feria.component';
+
 
 
 @Component({
@@ -14,11 +16,26 @@ export class FeriaModalComponent {
   @Input() feria: any;
   puestos: any[] = [];
   isAccordionOpen = false;
+  isComentariosAccordionOpen = true; // Abierto por defecto
   constructor(private modalCtrl: ModalController, private puestoService: PuestoService
   ) { }
 
   async ngOnInit() {
     await this.cargarPuestos();
+  }
+
+  toggleComentariosAccordion() {
+    this.isComentariosAccordionOpen = !this.isComentariosAccordionOpen;
+  }
+
+  async abrirModalComentario() {
+    const modal = await this.modalCtrl.create({
+      component: ComentarFeriaComponent,
+      componentProps: {
+        feriaId: this.feria.id
+      }
+    });
+    return await modal.present();
   }
 
   async abrirModalDetallePuesto(puestoId: number) {
